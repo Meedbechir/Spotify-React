e1bd57f34dc709e69ec5d189a56c0b63751f28a7
MeedBechir <medbechirathie@gmail.com>
2023-05-20 10:21:21 +0000

    last one

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import Logo from '../Images/sp.png';
 
 const Header = () => {
   return (
     <header className="header">
       <div className="container-fluid p-0">
         <nav className="d-flex justify-content-center align-items-center py-4 m-0">
+          <img src={Logo} alt="" className="header-logo" />
           <div className="ms-auto">
-            <button className="btn text-light ms-5">Sign Up</button>
-            <button className="btn btn-light me-5 bg-white">Log In</button>
+            <button className="btn text-white me-2">Sign Up</button>
+            <button className="btn btn-outline-light me-1">Log In</button>
           </div>
         </nav>
       </div>
